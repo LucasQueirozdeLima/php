9369452b77c3262c454efcb39a5a0fdbddb84cbb
lucas <lucas.lima716@etec.sp.gov.br>
2024-03-07 20:24:53 -0300

    formulario

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo</title>
+</head>
+<body>
+
+    <form action="acao.php" method="post">
+        <pre>
+
+        <label for="">Nome</label>
+        <input type="text" name="nome" id="">
+
+        <label for="">Telefone</label>
+        <input type="text" name="telefone" id="">
+
+        <input type="submit" value="Enviar">    <input type="reset" value="LImpar">
+        </pre>
+    </form>
+
+<?php
+?>
+</body>
+</html>
